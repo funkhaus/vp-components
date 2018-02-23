796be90219fe5bb3fd38cfad1468ac67e266299a
Luke Waldner <luke@funkhaus.us>
2018-02-23 12:56:56 -0500

    pushing sticky directive

---
+++ src/v-sticky/sticky.vue
@@ -0,0 +1,26 @@
+<script>
+
+    export default {
+        inserted (el, binding) {
+
+            const offset = binding.arg
+
+            window.addEventListener('scroll', () => {
+
+                if (!el.getBoundingClientRect) return
+
+                const rect = el.getBoundingClientRect()
+                top = rect.top
+                if (top <= offset){
+                    let translateDist = (top - offset) * -1
+                    el.style['transform'] = 'translateY(' + tr + 'px)'
+                }
+                else {
+                    el.style['transform'] = ""
+                }
+            })
+        }
+    }
+
+</script>
+
